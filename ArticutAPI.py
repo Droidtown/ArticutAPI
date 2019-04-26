11fc0635c741d8167d039b0f2391c4f9068e092f
Eclair <eclair.c@droidtown.co>
2019-04-26 14:34:09 +0800

    完成 aArticut Class 框架

---
+++ ArticutAPI.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import requests
+
+class Articut:
+    def __init__(self, username="", apikey="", version="latest", level="lv2"):
+        self.username = username
+        self.apikey = apikey
+        self.version = version
+        self.level = level
+
+        self.url = "https://api.droidtown.co"
+
+    def __str__(self):
+        return "Articut API"
+
+    def parse(self, inputSTR, level=self.level):
+        url = "{}/Articut/API/".format(self.url)
+        payload = {"input_str": inputSTR,
+                   "username": self.username,
+                   "api_key": self.apikey,
+                   "version": self.version,
+                   "level": level}
+        result = requests.post(url, data=payload)
+        if result.status_code == 200:
+            result = result.json()
+            result["product"] = "{}/product/".format(self.url)
+            result["document"] = "{}/document/".format(self.url)
+        return result
+
+    def versions(self):
+        url = "{}/Articut/Versions/".format(self.url)
+        payload = {"username":  self.username,
+                   "api_key":   self.apikey}
+        result = requests.post(url, data=payload)
+        if result.status_code == 200:
+            result = result.json()
+            result["product"] = "{}/product/".format(self.url)
+            result["document"] = "{}/document/".format(self.url)
+        return result
+
+if __name__ == "__main__":
+    from pprint import pprint
+
+    inputSTR = "努力才能成功"
+    articut = Articut()
+
+    result = articut.parse(inputSTR)
+    pprint(result)
+
+    result = articut.versions()
+    pprint(result)
+
