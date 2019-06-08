2f1bd254c8e34dbe185d7203c8c3cbb481a8e23c
peter <peter.w@droidtown.co>
2019-06-09 00:37:05 +0800

    新增 getContentWordLIST() 以便取得斷詞結果中的 content word list。

--- ArticutAPI.py
+++ ArticutAPI.py
@@ -3,10 +3,16 @@
 
 import json
 import os
+import re
 import requests
 
 class Articut:
     def __init__(self, username="", apikey="", version="latest", level="lv2"):
+        '''
+        username = "你註冊時的 email。若留空，則會使用每日 10 萬字的公用帳號。
+        apikey = "您完成付費後取得的 apikey 值。若留空，則會使用每日 10 萬字的公用帳號。
+        '''
+
         self.url = "https://api.droidtown.co"
         self.username = username
         self.apikey = apikey
@@ -15,6 +21,14 @@ class Articut:
 
         self.userDefinedDictFILE = None
         self.fileSizeLimit = 1024 * 1024 * 10    # 10 MB
+
+        self.verbPat = "(?<=<ACTION_verb>)[^<]*?(?=</ACTION_verb>)"
+        self.nounPat = "(?<=<ENTITY_nounHead>)[^<]*?(?=</ENTITY_nounHead>)|(?<=<ENTITY_nouny>)[^<]*?(?=</ENTITY_nouny>)|(?<=<ENTITY_noun>)[^<]*?(?=</ENTITY_noun>)|(?<=<ENTITY_oov>)[^<]*?(?=</ENTITY_oov>)"
+        self.modifierPat = "(?<=<MODIFIER>)[^<]*?(?=</MODIFIER>)"
+        self.funcPat = "(?<=<AUX>)[^<]*?(?=</AUX>)|(?<=<FUNC_in[nt]er>)[^<]*?(?=</FUNC_in[nt]er>)|(?<=<RANGE_locality>)[^<]*?(?=</RANGE_locality>)|(?<=<RANGE_period>)[^<]*?(?=</RANGE_period>)"
+        self.personPat = "(?<=<ENTITY_person>)[^<]*?(?=</ENTITY_person>)|(?<=<ENTITY_pronoun>)[^<]*?(?=</ENTITY_pronoun>)"
+        self.locationPat = "(?<=<LOCATION>)[^<]*?(?=</LOCATION>)"
+        self.timePat = "(?<=<TIME_decade>)[^<]*?(?=</TIME_decade>)|(?<=<TIME_year>)[^<]*?(?=</TIME_year>)|(?<=<TIME_season>)[^<]*?(?=</TIME_season>)|(?<=<TIME_month>)[^<]*?(?=</TIME_month>)|(?<=<TIME_week>)[^<]*?(?=</TIME_week>)|(?<=<TIME_day>)[^<]*?(?=</TIME_day>)|(?<=<TIME_justtime>)[^<]*?(?=</TIME_justtime>)"
 
     def __str__(self):
         return "Articut API"
@@ -53,6 +67,23 @@ class Articut:
             result["document"] = "{}/document/".format(self.url)
         return result
 
+
+    def getContentWordLIST(self, parseResultDICT):
+        '''
+        取出斷詞結果中的 content word。
+        每個句子內的 content word 為一個 list.
+        '''
+        if "result_pos" in parseResultDICT:
+            pass
+        else:
+            return None
+        contentWordLIST = []
+        contentPat = "|".join([self.verbPat, self.nounPat, self.modifierPat])
+        for p in parseResultDICT["result_pos"]:
+            if len(p) > 1:
+                contentWordLIST.append([(c.start(), c.end(), c.group(0)) for c in reversed(list(re.finditer(contentPat, p)))])
+        return contentWordLIST
+
     def versions(self):
         url = "{}/Articut/Versions/".format(self.url)
         payload = {"username":  self.username,
@@ -67,11 +98,18 @@ class Articut:
 if __name__ == "__main__":
     from pprint import pprint
 
-    inputSTR = "我要重修計概"
+    inputSTR = "我的計劃是讓你計劃人類補完計劃。"
     articut = Articut()
 
-    result = articut.parse(inputSTR, userDefinedDictFILE="./UserDefinedFile.json")
+    #取得斷詞結果
+    result = articut.parse(inputSTR, level="lv1")
     pprint(result)
 
-    result = articut.versions()
-    pprint(result)
+    #列出所有的 content word.
+    contentWordLIST = articut.getContentWordLIST(result)
+    pprint(contentWordLIST)
+
+
+    #列出目前可使用的 Articut 版本選擇。通常版本號愈大，完成度愈高。
+    versions = articut.versions()
+    pprint(versions)
