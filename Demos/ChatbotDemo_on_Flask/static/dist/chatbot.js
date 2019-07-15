c4e01d8d47ee00b55ec183f57f40a7ac4aebea16
PeterWolf <peterwolf.wang@gmail.com>
2019-07-15 10:09:35 +0800

    更新 jquery 及其目錄位置。

--- Demos/ChatbotDemo_on_Flask/static/dist/chatbot.js
+++ Demos/ChatbotDemo_on_Flask/static/dist/chatbot.js
@@ -1,10 +1,12 @@
-$("#send").click(function(){
-    var inputSTR = $("#userInput").val();
-    if (inputSTR == ""){
-        alert("say something, will you?")
-    }
-    else{
-        $("#invisibleForm").append("<div class='inputLog'>Chatbot: "+inputSTR+"</div>")
-        $("#messages").append("<div class='inputLog'>Human: "+inputSTR+"</div>")
-    }
+$( document ).ready(function(){
+    $("#send").click(function(){
+        var inputSTR = $("#userInput").val();
+        if (inputSTR == ""){
+            alert("say something, will you?")
+        }
+        else{
+            $("#invisibleForm").append("<div class='inputLog'>Chatbot: "+inputSTR+"</div>")
+            $("#messages").append("<div class='inputLog'>Human: "+inputSTR+"</div>")
+        }
+    });
 });
