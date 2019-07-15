33fabd0314e870aff35efc4a9f4030e18557381c
PeterWolf <peterwolf.wang@gmail.com>
2019-07-15 09:56:53 +0800

    新增 ChatbotDemo 架構.

---
+++ Demos/ChatbotDemo_on_Flask/static/dist/chatbot.js
@@ -0,0 +1,10 @@
+$("#send").click(function(){
+    var inputSTR = $("#userInput").val();
+    if (inputSTR == ""){
+        alert("say something, will you?")
+    }
+    else{
+        $("#invisibleForm").append("<div class='inputLog'>Chatbot: "+inputSTR+"</div>")
+        $("#messages").append("<div class='inputLog'>Human: "+inputSTR+"</div>")
+    }
+});
