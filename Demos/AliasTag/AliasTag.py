c750a4db3ccf4864b23226bdec43460ba987602e
peter <peter.w@droidtown.co>
2019-07-17 22:45:04 +0800

    修正變數型別。

--- Demos/AliasTag/AliasTag.py
+++ Demos/AliasTag/AliasTag.py
@@ -13,20 +13,20 @@ atc = Articut()
 
 #Demo 用的文字：載入政府機構名稱前。
 inputSTR = "國軍退除役官兵輔導委員會簡稱退輔會。 "
-resultSTR = atc.parse(inputSTR)
+resultDICT = atc.parse(inputSTR)
 print("1. 政府機構名稱直接「斷詞」處理：")
-print(resultSTR["result_pos"])
+print(resultDICT["result_pos"])
 print("=====================")
 
 inputSTR = "國軍退除役官兵輔導委員會簡稱退輔會。 "
-resultSTR = atc.parse(inputSTR, userDefinedDictFILE="../../Public_UserDefinedDict/KNOWLEDGE_gov.json")
+resultDICT = atc.parse(inputSTR, userDefinedDictFILE="../../Public_UserDefinedDict/KNOWLEDGE_gov.json")
 print("2. 政府機構名稱用「自定字典」處理：")
-print(resultSTR["result_pos"])
+print(resultDICT["result_pos"])
 print("=====================")
 
 inputSTR = "國軍退除役官兵輔導委員會簡稱退輔會。 "
-resultSTR = atc.parse(inputSTR, userDefinedDictFILE="../../Public_UserDefinedDict/KNOWLEDGE_gov.json")
+resultDICT = atc.parse(inputSTR, userDefinedDictFILE="../../Public_UserDefinedDict/KNOWLEDGE_gov.json")
 print("3. 政府機構名稱用「自定字典」處理，再依字典名稱調整「標記」。")
 tagName = "../../Public_UserDefinedDict/KNOWLEDGE_gov.json".split("/")[-1].split(".")[0] #取得 KNOWLEDGE_gov
-print([result.replace("UserDefined", tagName) for result in resultSTR["result_pos"]])
+print([result.replace("UserDefined", tagName) for result in resultDICT["result_pos"]])
 print("=====================")
