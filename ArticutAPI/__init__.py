103c0302483065f953a46572f754455693d2e9ce
Eclair <eclair.c@droidtown.co>
2021-06-07 15:37:18 +0800

    更新預載 module

--- ArticutAPI/__init__.py
+++ ArticutAPI/__init__.py
@@ -1,12 +1,12 @@
 try:
     from .ArticutAPI import Articut
-    from Toolkit.analyse import AnalyseManager
-    from Toolkit.localRE import TaiwanAddressAnalizer
-    from Toolkit.toolkits import *
-    from Toolkit.NER import GenericNER
-except:
-    from ArticutAPI import Articut
     from .Toolkit.analyse import AnalyseManager
     from .Toolkit.localRE import TaiwanAddressAnalizer
     from .Toolkit.toolkits import *
     from .Toolkit.NER import GenericNER
+except:
+    from ArticutAPI import Articut
+    from Toolkit.analyse import AnalyseManager
+    from Toolkit.localRE import TaiwanAddressAnalizer
+    from Toolkit.toolkits import *
+    from Toolkit.NER import GenericNER
