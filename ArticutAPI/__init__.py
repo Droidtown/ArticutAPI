7bf31d315727a7631fcb5bd9af43b4cc0a8e306c
Eclair <eclair.c@droidtown.co>
2021-06-07 15:33:58 +0800

    調整 localRE roomPat, 更新預載 module

--- ArticutAPI/__init__.py
+++ ArticutAPI/__init__.py
@@ -1,6 +1,12 @@
 try:
     from .ArticutAPI import Articut
-    from .Toolkit.toolkits import LawsToolkit
+    from Toolkit.analyse import AnalyseManager
+    from Toolkit.localRE import TaiwanAddressAnalizer
+    from Toolkit.toolkits import *
+    from Toolkit.NER import GenericNER
 except:
     from ArticutAPI import Articut
-    from Toolkit.toolkits import LawsToolkit
+    from .Toolkit.analyse import AnalyseManager
+    from .Toolkit.localRE import TaiwanAddressAnalizer
+    from .Toolkit.toolkits import *
+    from .Toolkit.NER import GenericNER
